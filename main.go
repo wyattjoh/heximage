759185617d10e92ea01c5c2fdb578451da69d9f1
Wyatt Johnson <wyattjoh@gmail.com>
2017-04-17 17:15:05 -0600

    Added webserver

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/png"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/garyburd/redigo/redis"
 	"github.com/pkg/errors"
 )
 
 const (
+	// timeout is the cache timeout used to add to requests to prevent the
+	// browser from re-requesting the image.
+	timeout = 30 * time.Second
+
 	key    = "heximage"
 	width  = 1000
 	height = 1000
@@ -60,10 +68,10 @@ func SendSet(conn redis.Conn, x, y, colour uint32) error {
 }
 
 // GetImage returns an image.
-func GetImage(conn redis.Conn) error {
+func GetImage(conn redis.Conn) (image.Image, error) {
 	data, err := redis.Bytes(conn.Do("GET", key))
 	if err != nil {
-		return errors.Wrap(err, "can't get the image bytes")
+		return nil, errors.Wrap(err, "can't get the image bytes")
 	}
 
 	if len(data) != bits {
@@ -76,15 +84,7 @@ func GetImage(conn redis.Conn) error {
 
 	img.Pix = data
 
-	enc := png.Encoder{
-		CompressionLevel: png.BestCompression,
-	}
-
-	if err := enc.Encode(os.Stdout, img); err != nil {
-		return err
-	}
-
-	return nil
+	return img, nil
 }
 
 // ClearImage clears the image.
@@ -144,6 +144,81 @@ func usage() {
 	os.Exit(1)
 }
 
+// StartServer runs an http server capable of serving requests for the image
+// service.
+func StartServer(pool *redis.Pool) error {
+
+	http.HandleFunc("/api/place/draw", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		if r.Method != "POST" {
+			log.Printf("%s /api/place/draw %d - %s", r.Method, http.StatusMethodNotAllowed, time.Since(start))
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		conn := pool.Get()
+		defer conn.Close()
+
+		var pl struct {
+			X, Y, Colour string
+		}
+		if err := json.NewDecoder(r.Body).Decode(&pl); err != nil {
+			log.Printf("%s /api/place/draw %d - %s", r.Method, http.StatusBadRequest, time.Since(start))
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		if err := SetColour(conn, pl.X, pl.Y, pl.Colour); err != nil {
+			log.Printf("%s /api/place/draw %d - %s", r.Method, http.StatusInternalServerError, time.Since(start))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("%s /api/place/draw %d - %s", r.Method, http.StatusOK, time.Since(start))
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	http.HandleFunc("/api/place/board-bitmap", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		if r.Method != "GET" {
+			log.Printf("%s /api/place/board-bitmap %d - %s", r.Method, http.StatusMethodNotAllowed, time.Since(start))
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		conn := pool.Get()
+		defer conn.Close()
+
+		img, err := GetImage(conn)
+		if err != nil {
+			log.Printf("%s /api/place/board-bitmap %d - %s", r.Method, http.StatusInternalServerError, time.Since(start))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Cache-Control", fmt.Sprintf("public, max-age=%d", int64(timeout.Seconds())))
+		w.Header().Set("Last-Modified", time.Now().String())
+
+		enc := png.Encoder{
+			CompressionLevel: png.BestSpeed,
+		}
+
+		if err := enc.Encode(w, img); err != nil {
+			log.Printf("%s /api/place/board-bitmap %d - %s", r.Method, http.StatusInternalServerError, time.Since(start))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("%s /api/place/board-bitmap %d - %s", r.Method, http.StatusOK, time.Since(start))
+	})
+
+	log.Printf("Now listening on 127.0.0.1:8080")
+	return http.ListenAndServe("127.0.0.1:8080", nil)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		usage()
@@ -155,6 +230,14 @@ func main() {
 		os.Exit(1)
 	}
 	defer pool.Close()
+
+	switch os.Args[1] {
+	case "server":
+		if err := StartServer(pool); err != nil {
+			fmt.Printf("Can't run the server: %s\n", err.Error())
+			os.Exit(1)
+		}
+	}
 
 	conn := pool.Get()
 	defer conn.Close()
@@ -176,8 +259,18 @@ func main() {
 			os.Exit(1)
 		}
 	case "get":
-		if err := GetImage(conn); err != nil {
+		img, err := GetImage(conn)
+		if err != nil {
 			fmt.Printf("Can't get the image: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		enc := png.Encoder{
+			CompressionLevel: png.BestCompression,
+		}
+
+		if err := enc.Encode(os.Stdout, img); err != nil {
+			fmt.Printf("Can't encode the image: %s\n", err.Error())
 			os.Exit(1)
 		}
 	case "test":
@@ -190,6 +283,7 @@ func main() {
 			fmt.Printf("Can't clear the image: %s\n", err.Error())
 			os.Exit(1)
 		}
+
 	default:
 		usage()
 	}
