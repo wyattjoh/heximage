291242d491eb231982f7a69200db8ebe3c6a8ecb
Wyatt Johnson <wyattjoh@gmail.com>
2017-04-18 09:21:11 -0600

    Added more logs

--- lib/redis.go
+++ lib/redis.go
@@ -4,7 +4,9 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/garyburd/redigo/redis"
+	"github.com/pkg/errors"
 )
 
 // ConnectRedis connects to the redis instance, pings it, and returns the pool
@@ -14,7 +16,7 @@ func ConnectRedis(dsn string, maxActive int) (*redis.Pool, error) {
 	// Parse the dsn down to a URL.
 	addr, err := url.Parse(dsn)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "can't parse the redis server dsn")
 	}
 
 	// Create the pool.
@@ -25,15 +27,17 @@ func ConnectRedis(dsn string, maxActive int) (*redis.Pool, error) {
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", addr.Host)
 			if err != nil {
-				return nil, err
+				return nil, errors.Wrap(err, "can't dial the redis server")
 			}
 			if addr.User != nil {
 				password, _ := addr.User.Password()
 				if password != "" {
 					if _, err = c.Do("AUTH", password); err != nil {
 						c.Close()
-						return nil, err
+						return nil, errors.Wrap(err, "can't auth to the redis server")
 					}
+
+					logrus.WithField("query", "redis").Debugf("AUTH <redacted>")
 				}
 			}
 			return c, err
@@ -45,8 +49,13 @@ func ConnectRedis(dsn string, maxActive int) (*redis.Pool, error) {
 
 			// Test that we can ping if the last time we tested this connection was
 			// more than 1 minute ago.
-			_, err := c.Do("PING")
-			return err
+			if _, err := c.Do("PING"); err != nil {
+				return errors.Wrap(err, "can't ping redis server after timeout exhausted")
+			}
+
+			logrus.WithField("query", "redis").Debugf("PING")
+
+			return nil
 		},
 	}
 
@@ -56,8 +65,10 @@ func ConnectRedis(dsn string, maxActive int) (*redis.Pool, error) {
 
 	// Ensure we can ping the server.
 	if _, err := con.Do("PING"); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "can't ping the redis server for the first time")
 	}
+
+	logrus.WithField("query", "redis").Debugf("PING")
 
 	return &pool, nil
 }
