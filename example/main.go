5c1b1c85c4d4bc1deb8bac1967bf390cdbb2f14f
Wyatt Johnson <wyattjoh@gmail.com>
2017-04-18 08:15:29 -0600

    improved example

--- example/main.go
+++ example/main.go
@@ -11,13 +11,20 @@ import (
 	heximage "github.com/wyattjoh/heximage/lib"
 )
 
+const (
+	bindIP   = "localhost"
+	apiBind  = bindIP + ":8080"
+	temBind  = bindIP + ":8000"
+	redisURL = "redis://localhost:6379"
+)
+
 func main() {
 
 	// Load the templates.
 	tmpl := template.Must(template.ParseFiles(filepath.Join("example", "index.html")))
 
 	// Create the redis pool, assuming a default here.
-	pool, err := heximage.ConnectRedis("redis://localhost:6379", 0)
+	pool, err := heximage.ConnectRedis(redisURL, 0)
 	if err != nil {
 		fmt.Println("Can't connect to redis", err)
 		os.Exit(1)
@@ -28,19 +35,26 @@ func main() {
 	conn := pool.Get()
 	defer conn.Close()
 
-	const heximageBind = "localhost:8080"
-
 	// Start the heximage server.
-	go heximage.StartServer(heximageBind, pool, conn, []string{"http://localhost:8000"})
+	go func() {
+		log.Printf("Now serving the api on %s", apiBind)
+		if err := heximage.StartServer(apiBind, pool, conn, []string{temBind}); err != nil {
+			fmt.Println("Can't serve the api server", err)
+			os.Exit(1)
+		}
+	}()
 
 	// Prepare to serve the index.html file from the example directory.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, map[string]interface{}{
-			"Origin": heximageBind,
+			"Origin": apiBind,
 		})
 	})
 
 	// Start the server.
-	log.Println("Now listening for requests on localhost:8000")
-	http.ListenAndServe("localhost:8000", nil)
+	log.Printf("Now serving the template on %s", temBind)
+	if err := http.ListenAndServe(temBind, nil); err != nil {
+		fmt.Println("Can't serve the template server", err)
+		os.Exit(1)
+	}
 }
